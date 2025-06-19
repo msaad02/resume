bbaba1d225390c71d500ff74814887aa3a1f28b9
Matthew Saad <msaad02@outlook.com>
2025-06-19 12:59:41 -0400

    add msaad_ prefix to resumes

--- template/build.sh
+++ template/build.sh
@@ -9,27 +9,27 @@ mkdir -p $TMPFILES
 
 cp template/resume.tex $TMPFILES/resume_build.tex
 
-# Replace the default role include with the selected role file.
-# GNU sed (used on Linux) doesn't require a backup suffix for -i.
-# The previous "-i ''" syntax was BSD-specific and caused errors on Linux.
+# Replace \input{} with correct role.tex path
 sed -i "s|\\\input{roles/.*}|\\\input{../roles/${ROLE}.tex}|g" \
     "$TMPFILES/resume_build.tex"
 
-# Run latexmk: send all aux output to $TMPDIR, set jobname
+# Run latexmk to create PDF
 latexmk -pdf -silent \
         -jobname="$JOBNAME" \
         -outdir="$TMPFILES" \
         $TMPFILES/resume_build.tex
 
+# Check sucessful creation, if not, error out
 if [ $? -ne 0 ]; then
     echo "LaTeX build failed."
-    # rm -rf $TMPFILES
+    rm -rf $TMPFILES
     exit 1
 fi
 
 # Move the finished PDF where we want it
-mv "${TMPFILES}/${JOBNAME}.pdf" "$OUTDIR/${JOBNAME}.pdf"
+mv "${TMPFILES}/${JOBNAME}.pdf" "$OUTDIR/msaad_${JOBNAME}.pdf"
 
+# Get rid of logfiles, ..., junk. Useful for local dev
 rm -rf $TMPFILES
 
 echo "Successfully built $OUTDIR/${JOBNAME}.pdf"
